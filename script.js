f296cbfc91ce8d1ef8c4672ad49c2bfaec7a6ed9
Menezndri2023 <ndrimanasse@gmail.com>
2023-10-09 11:15:15 +0100

    presque terminer

---
+++ script.js
@@ -0,0 +1,10 @@
+const zero = document.querySelector("#zero")
+const panier = document.querySelectorAll(".panier")
+
+zero.addEventListener('click',function(){
+    console.log(zero)
+});
+// panier.addEventListener('click',function(){
+//     // console.log(panier)
+// zero.innerText++
+// });
